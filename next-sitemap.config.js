d5ef259c99209ffa808c3296d7a3734c77ebb169
ultrocharge <oliver.elias011@gmail.com>
2024-10-31 15:42:45 +0000

    init commit

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://example.com',
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  robotsTxtOptions: {
+    policies: [{ userAgent: '*', allow: '/' }],
+  },
+};
